90f2f10699dcf64f56b1b61f32938b48a50e60b4
Gepiro <gepiro0916@gmail.com>
2021-04-19 19:43:20 +0200

    Fix cellrangerIndexing

--- cellrangerIndexing/indexing.sh
+++ cellrangerIndexing/indexing.sh
@@ -5,9 +5,10 @@ wget $2 -O /data/genome.fa.gz
 gzip -d /data/genome.gtf.gz
 gzip -d /data/genome.fa.gz
 
-/bin/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:$3
+/bin/cellranger-5.0.1/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:$3
+#/bin/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:$3
 #/bin/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:bio_type
 cd /data
-/bin/cellranger mkref --genome=10XGenome --fasta=/data/genome.fa --genes=/data/output.gtf --nthreads=4
+/bin/cellranger-5.0.1/cellranger mkref --genome=10XGenome --fasta=/data/genome.fa --genes=/data/output.gtf --nthreads=4
 
 rm /data/output.gtf
