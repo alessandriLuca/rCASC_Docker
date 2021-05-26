d45c8252dbb8707e4ba93903eb70fad069a482be
Gepiro <gepiro0916@gmail.com>
2021-05-26 15:39:36 +0200

    Fixed Dockerfile cellrangerCount

--- cellrangerIndexing/indexing.sh
+++ cellrangerIndexing/indexing.sh
@@ -5,10 +5,10 @@ wget $2 -O /data/genome.fa.gz
 gzip -d /data/genome.gtf.gz
 gzip -d /data/genome.fa.gz
 
-/bin/cellranger-5.0.1/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:$3
+/bin/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:$3
 #/bin/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:$3
 #/bin/cellranger mkgtf /data/genome.gtf /data/output.gtf --attribute=gene_biotype:bio_type
 cd /data
-/bin/cellranger-5.0.1/cellranger mkref --genome=10XGenome --fasta=/data/genome.fa --genes=/data/output.gtf --nthreads=4
+/bin/cellranger mkref --genome=10XGenome --fasta=/data/genome.fa --genes=/data/output.gtf --nthreads=4
 
 rm /data/output.gtf
