ba1425fdafb5abbe01de7d2ae172aedecb34609f
Gepiro <gepiro0916@gmail.com>
2021-05-26 15:41:54 +0200

    Update example cellrangerIndexing

--- cellrangerIndexing/Example/example_Script.R
+++ cellrangerIndexing/Example/example_Script.R
@@ -2,6 +2,6 @@ library(rCASC)
 path=getwd()
 
 cellrangerIndexing(group="docker", scratch.folder=path, 
-            gtf.url="ftp://ftp.ensembl.org/pub/release-87/gtf/homo_sapiens/Homo_sapiens.GRCh38.87.gtf.gz",
-            fasta.url="ftp://ftp.ensembl.org/pub/release-87/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.toplevel.fa.gz",
+            gtf.url="http://ftp.ensembl.org/pub/release-87/gtf/homo_sapiens/Homo_sapiens.GRCh38.87.gtf.gz",
+            fasta.url="http://ftp.ensembl.org/pub/release-87/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.toplevel.fa.gz",
             genomeFolder = getwd(), bio.type="protein_coding", nThreads = 8)
